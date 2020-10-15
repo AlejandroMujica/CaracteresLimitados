6b23a3407d7a4b9fe91fe599808fc9e392078b2b
Alejandro Mujica <alejandro.mujica@iesdoctorbalmis.com>
2020-10-15 17:00:56 +0200

    Agregar archivos de proyecto.

---
+++ CaracteresLimitados/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Windows;
+
+
+namespace CaracteresLimitados
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void limitadoTextBox_TextChanged(object sender, RoutedEventArgs e)
+        {
+            contadorLetrasTextBlock.Text = limitadoTextBox.Text.Length + "/140";
+
+            if(limitadoTextBox.Text.Length == 140)
+            {
+                limitadoTextBox.IsReadOnly=true;
+            }
+        }
+    }
+}
